c7629865fbce1a95441bbf76a4604d55541d0ae9
NarineHay <naromisho87@gmail.com>
2025-09-09 05:50:20 +0400

    2

---
+++ resources/js/Stores/compare.js
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia';
+import { useModalStore } from '@/Stores/modalStore'
+
+
+export const useCompareStore = defineStore('compare', {
+
+    state: () => ({
+        list: JSON.parse(localStorage.getItem('compare_products') || '[]'),
+    }),
+    actions: {
+        save() {
+            localStorage.setItem('compare_products', JSON.stringify(this.list));
+        },
+        add(productId) {
+
+
+            if (this.list.includes(productId)) {
+                console.log('Уже в списке');
+                return;
+            }
+            if (this.list.length >= 3) {
+                const modal = useModalStore();
+
+                return;
+            }
+            this.list.push(productId);
+            this.save();
+            console.log('Текущий список:', this.list);
+        },
+        remove(productId) {
+            this.list = this.list.filter(id => id !== productId);
+            this.save();
+        },
+        clear() {
+            this.list = [];
+            this.save();
+        }
+    },
+    getters: {
+        count: (state) => state.list.length,
+        ids: (state) => state.list,
+    }
+});
