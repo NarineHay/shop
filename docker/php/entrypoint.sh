ebeac01e0e14197fd1fec05cd350e5bbb889e444
Narine <naromisho87@gmail.com>
2025-12-20 20:51:01 +0400

    docker

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 set -e
-
-echo "📁 Fixing storage permissions..."
-
-mkdir -p \
-  storage/framework/cache \
-  storage/framework/sessions \
-  storage/framework/views \
-  bootstrap/cache
-
+# Устанавливаем права
 chown -R www-data:www-data storage bootstrap/cache
-chmod -R 775 storage bootstrap/cache
+
+# Генерируем ключ, если нет
+if [ ! -f /var/www/.env ]; then
+  cp /var/www/.env.example /var/www/.env
+  php artisan key:generate
+fi
+
 
 exec "$@"
