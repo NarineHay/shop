8003d6744af88ea74513d5ac5017dd200b796ed9
Narine <naromisho87@gmail.com>
2026-01-02 21:09:35 +0400

    seeder attributes

--- database/seeders/AttributeSeeder.php
+++ database/seeders/AttributeSeeder.php
@@ -6,40 +6,195 @@ use App\Models\Attribute;
 use App\Models\AttributeTranslation;
 use App\Models\AttributeValue;
 use App\Models\AttributeValueTranslation;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class AttributeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $attributes = [
+
+            /*
+            |--------------------------------------------------------------------------
+            | COMMERCIAL ATTRIBUTES
+            |--------------------------------------------------------------------------
+            */
+
             'color' => [
+                'type' => 'select',
+                'translations' => [
+                    'hy' => 'Գույն',
+                    'en' => 'Color',
+                    'ru' => 'Цвет',
+                ],
+                'values' => [
+                    ['code' => 'black', 'translations' => [
+                        'hy' => 'Սև',
+                        'en' => 'Black',
+                        'ru' => 'Чёрный',
+                    ]],
+                    ['code' => 'white', 'translations' => [
+                        'hy' => 'Սպիտակ',
+                        'en' => 'White',
+                        'ru' => 'Белый',
+                    ]],
+                    ['code' => 'grey', 'translations' => [
+                        'hy' => 'Մոխրագույն',
+                        'en' => 'Grey',
+                        'ru' => 'Серый',
+                    ]],
+                    ['code' => 'blue', 'translations' => [
+                        'hy' => 'Կապույտ',
+                        'en' => 'Blue',
+                        'ru' => 'Синий',
+                    ]],
+                ],
+            ],
+
+            'product_type' => [
+                'type' => 'select',
+                'translations' => [
+                    'hy' => 'Ապրանքի տեսակ',
+                    'en' => 'Product Type',
+                    'ru' => 'Тип товара',
+                ],
+                'values' => [
+                    ['code' => 'variable_speed_drive', 'translations' => [
+                        'hy' => 'Հաճախականության փոխարկիչ',
+                        'en' => 'Variable Speed Drive',
+                        'ru' => 'Частотный преобразователь',
+                    ]],
+                    ['code' => 'automatic_transfer_switch', 'translations' => [
+                        'hy' => 'Ավտոմատ ռեզերվի մուտք',
+                        'en' => 'Automatic Transfer Switch',
+                        'ru' => 'Автоматический ввод резерва',
+                    ]],
+                ],
+            ],
+
+            /*
+            |--------------------------------------------------------------------------
+            | TECHNICAL ATTRIBUTES
+            |--------------------------------------------------------------------------
+            */
+
+            'motor_power_kw' => [
+                'type' => 'float',
+                'translations' => [
+                    'hy' => 'Շարժիչի հզորություն (կՎտ)',
+                    'en' => 'Motor Power (kW)',
+                    'ru' => 'Мощность двигателя (кВт)',
+                ],
+            ],
+
+            'motor_power_hp' => [
+                'type' => 'float',
+                'translations' => [
+                    'hy' => 'Շարժիչի հզորություն (hp)',
+                    'en' => 'Motor Power (hp)',
+                    'ru' => 'Мощность двигателя (л.с.)',
+                ],
+            ],
+
+            'supply_voltage' => [
                 'type' => 'string',
-                'translations' => ['hy' => 'Գույն', 'en' => 'Color', 'ru' => 'Цвет'],
-                'values' => [
-                    ['code' => 'red', 'translations' => ['hy' => 'Կարմիր', 'en' => 'Red', 'ru' => 'Красный']],
-                    ['code' => 'blue', 'translations' => ['hy' => 'Կապույտ', 'en' => 'Blue', 'ru' => 'Синий']],
+                'translations' => [
+                    'hy' => 'Լարման մատակարարում',
+                    'en' => 'Supply Voltage',
+                    'ru' => 'Напряжение питания',
                 ],
             ],
-            'battery_capacity' => [
+
+            'phases' => [
                 'type' => 'integer',
-                'translations' => ['hy' => 'Մարտկոցի տարողություն', 'en' => 'Battery Capacity', 'ru' => 'Ёмкость батареи'],
-                'values' => [
-                    ['code' => 'b3000', 'translations' => ['hy' => '3000 mAh', 'en' => '3000 mAh', 'ru' => '3000 мАч']],
-                    ['code' => 'b5000', 'translations' => ['hy' => '5000 mAh', 'en' => '5000 mAh', 'ru' => '5000 мАч']],
+                'translations' => [
+                    'hy' => 'Փուլերի քանակ',
+                    'en' => 'Number of Phases',
+                    'ru' => 'Количество фаз',
                 ],
             ],
-            'weight' => [
-                'type' => 'integer',
-                'translations' => ['hy' => 'Կշիռ', 'en' => 'Weight', 'ru' => 'Вес'],
+
+            'supply_frequency' => [
+                'type' => 'string',
+                'translations' => [
+                    'hy' => 'Հաճախականություն',
+                    'en' => 'Frequency',
+                    'ru' => 'Частота',
+                ],
+            ],
+
+            'communication_protocol' => [
+                'type' => 'select',
+                'translations' => [
+                    'hy' => 'Կապի արձանագրություն',
+                    'en' => 'Communication Protocol',
+                    'ru' => 'Протокол связи',
+                ],
                 'values' => [
-                    ['code' => 'w150', 'translations' => ['hy' => '150 գ', 'en' => '150 g', 'ru' => '150 г']],
-                    ['code' => 'w200', 'translations' => ['hy' => '200 գ', 'en' => '200 g', 'ru' => '200 г']],
+                    ['code' => 'modbus', 'translations' => [
+                        'hy' => 'Modbus',
+                        'en' => 'Modbus',
+                        'ru' => 'Modbus',
+                    ]],
+                ],
+            ],
+
+            'cooling_type' => [
+                'type' => 'select',
+                'translations' => [
+                    'hy' => 'Սառեցման տեսակ',
+                    'en' => 'Cooling Type',
+                    'ru' => 'Тип охлаждения',
+                ],
+                'values' => [
+                    ['code' => 'fan', 'translations' => [
+                        'hy' => 'Օդային (Fan)',
+                        'en' => 'Fan',
+                        'ru' => 'Вентилятор',
+                    ]],
+                ],
+            ],
+
+            'ip_rating' => [
+                'type' => 'select',
+                'translations' => [
+                    'hy' => 'Պաշտպանության աստիճան',
+                    'en' => 'IP Rating',
+                    'ru' => 'Степень защиты',
+                ],
+                'values' => [
+                    ['code' => 'ip20', 'translations' => [
+                        'hy' => 'IP20',
+                        'en' => 'IP20',
+                        'ru' => 'IP20',
+                    ]],
+                ],
+            ],
+
+            'width_mm' => [
+                'type' => 'integer',
+                'translations' => [
+                    'hy' => 'Լայնություն (մմ)',
+                    'en' => 'Width (mm)',
+                    'ru' => 'Ширина (мм)',
+                ],
+            ],
+
+            'height_mm' => [
+                'type' => 'integer',
+                'translations' => [
+                    'hy' => 'Բարձրություն (մմ)',
+                    'en' => 'Height (mm)',
+                    'ru' => 'Высота (мм)',
+                ],
+            ],
+
+            'depth_mm' => [
+                'type' => 'float',
+                'translations' => [
+                    'hy' => 'Խորություն (մմ)',
+                    'en' => 'Depth (mm)',
+                    'ru' => 'Глубина (мм)',
                 ],
             ],
         ];
@@ -52,22 +207,33 @@ class AttributeSeeder extends Seeder
 
             foreach ($data['translations'] as $locale => $name) {
                 AttributeTranslation::updateOrCreate(
-                    ['attribute_id' => $attribute->id, 'locale' => $locale],
+                    [
+                        'attribute_id' => $attribute->id,
+                        'locale' => $locale,
+                    ],
                     ['name' => $name]
                 );
             }
 
-            foreach ($data['values'] as $val) {
-                $value = AttributeValue::updateOrCreate(
-                    ['attribute_id' => $attribute->id, 'code' => $val['code']],
-                    []
-                );
-
-                foreach ($val['translations'] as $locale => $valueName) {
-                    AttributeValueTranslation::updateOrCreate(
-                        ['attribute_value_id' => $value->id, 'locale' => $locale],
-                        ['name' => $valueName]
+            if (!empty($data['values'])) {
+                foreach ($data['values'] as $val) {
+                    $value = AttributeValue::updateOrCreate(
+                        [
+                            'attribute_id' => $attribute->id,
+                            'code' => $val['code'],
+                        ],
+                        []
                     );
+
+                    foreach ($val['translations'] as $locale => $valueName) {
+                        AttributeValueTranslation::updateOrCreate(
+                            [
+                                'attribute_value_id' => $value->id,
+                                'locale' => $locale,
+                            ],
+                            ['name' => $valueName]
+                        );
+                    }
                 }
             }
         }
